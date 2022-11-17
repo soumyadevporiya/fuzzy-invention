2f35d4ddc9583b432e54c4033d8bf39a57424e8d
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2022-11-17 19:37:28 +0530

    First Committ

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.7-slim
+WORKDIR ./
+COPY ./requirement.txt ./requirement.txt
+RUN pip install -r requirement.txt
+COPY ./sc_microservice_1.py ./sc_microservice_1.py
---
+++ sc_microservice_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+from flask import Flask
+
+
+# In[7]:
+
+
+app = Flask(__name__)
+
+
+# In[8]:
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+# In[12]:
+
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' %name
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    app.run(port=5003)
+
+
+# In[ ]:
+
+
+
+
